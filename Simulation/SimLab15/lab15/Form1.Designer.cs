cbcc04c02e2d64aa773f11057fee7c95404d48f9
TheAffeqt <57591944+TheAffeqt@users.noreply.github.com>
2020-06-13 18:56:15 +0700

    Lab15

---
+++ Simulation/SimLab15/lab15/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace lab15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Find = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Pause = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Compare = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Find
+            // 
+            this.Find.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Find.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Find.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Find.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Find.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Find.Location = new System.Drawing.Point(222, 34);
+            this.Find.Name = "Find";
+            this.Find.Size = new System.Drawing.Size(219, 42);
+            this.Find.TabIndex = 0;
+            this.Find.Text = "Find out the weather";
+            this.Find.UseVisualStyleBackColor = false;
+            this.Find.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(44, 82);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(629, 237);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Pause
+            // 
+            this.Pause.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Pause.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Pause.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Pause.Location = new System.Drawing.Point(419, 356);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(124, 32);
+            this.Pause.TabIndex = 5;
+            this.Pause.Text = "Stop";
+            this.Pause.UseVisualStyleBackColor = false;
+            this.Pause.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(44, 421);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+            series2.ChartArea = "ChartArea1";
+            series2.IsValueShownAsLabel = true;
+            series2.IsXValueIndexed = true;
+            series2.LabelFormat = "f3";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.LabelBorderWidth = 3;
+            series3.Legend = "Legend1";
+            series3.Name = "Series2";
+            this.chart2.Series.Add(series2);
+            this.chart2.Series.Add(series3);
+            this.chart2.Size = new System.Drawing.Size(629, 300);
+            this.chart2.TabIndex = 6;
+            this.chart2.Text = "chart2";
+            // 
+            // Compare
+            // 
+            this.Compare.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Compare.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Compare.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Compare.Location = new System.Drawing.Point(106, 356);
+            this.Compare.Name = "Compare";
+            this.Compare.Size = new System.Drawing.Size(128, 32);
+            this.Compare.TabIndex = 7;
+            this.Compare.Text = "Compare";
+            this.Compare.UseVisualStyleBackColor = false;
+            this.Compare.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 738);
+            this.Controls.Add(this.Compare);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.Find);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Find;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button Compare;
+    }
+}
+
---
+++ Simulation/SimLab15/lab15/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab15
+{
+    public partial class Form1 : Form
+    {
+        Weather weather;
+        Random rnd = new Random();
+        
+        int modeling;
+        
+        double a, time;
+        double[] Q;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            weather = new Weather();
+            Q = new double[3] { -0.4, -0.8, -0.5 };
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            time = 0;
+            modeling = 0;
+            
+            chart1.Series[0].Points.Clear();
+            timer1.Start();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            chart2.Series[0].Points.Clear();
+            chart2.Series[1].Points.Clear();
+            weather.GetStat();
+
+            for (int i = 0; i < 3; i++)
+            {
+                chart2.Series[0].Points.AddXY(i + 1, weather.Freq[i]);
+                chart2.Series[1].Points.AddXY(i + 1, weather.Theory[i]);
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.AddXY((int)time, modeling + 1);
+            a = rnd.NextDouble();
+
+            time += Math.Log(a) / Q[modeling];
+            modeling = weather.WeatherModeling(a);
+        }
+
+    }
+
+    public class Weather
+    {
+        int modeling = 0;
+        int N = 1000;
+        int T = 1000;
+        
+        Random rnd = new Random();
+        
+        double a;
+        double[,] Q = new double[3, 3] { { -0.4, 0.3, 0.1 }, { 0.4, -0.8, 0.4 }, { 0.1, 0.4, -0.5 } };
+        public double[] Freq = new double[3];
+        public double[] Theory = new double[3];
+
+        
+
+        public int WeatherModeling(double a)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (modeling != i)
+                    a -= Q[modeling, i] / Math.Abs(Q[modeling, modeling]);
+                else
+                    a -= 0;
+                
+                if (a < 0)
+                {
+                    modeling = i;
+                    return modeling;
+                }
+            }
+            
+            return modeling;
+        }
+        
+        public int Gen()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (modeling != i)
+                    a -= Q[modeling, i] / Math.Abs(Q[modeling, modeling]);
+                else
+                    a -= 0;
+                
+                if (a < 0)
+                {
+                    modeling = i;
+                    return modeling;
+                }
+            }
+            
+            return modeling;
+        }
+
+        public void GetStat()
+        {
+            modeling = 0;
+            
+            for (int i = 0; i < 3; i++)
+            {
+                Freq[i] = 0;
+                Theory[i] = 0;
+            }
+            
+            p();
+            
+            for (int i = 0; i < N; i++)
+            {
+                for (double j = 0; j < T;)
+                {
+                    a = rnd.NextDouble();
+                    Gen();
+                    j += Math.Log(a) / Q[modeling, modeling];
+                }
+                Freq[modeling] = Freq[modeling] + 1;
+            }
+            
+            for (int i = 0; i < 3; i++) Freq[i] /= N;
+        }
+
+        public void p()
+        {
+            for(int i=0; i<3; i++)
+            Theory[i]=(-Q[i,i])*Math.Exp(-(-Q[i, i]));
+        }
+
+    }
+}
